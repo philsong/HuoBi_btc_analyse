4d89c0567609949cce2c2024bb79c1e189b4bf01
yacc <origingod.oh@gmail.com>
2014-10-24 22:43:28 +0800

    api client

--- back_test.py
+++ back_test.py
@@ -7,36 +7,38 @@ import numpy as np
 import sklearn
 from sklearn.metrics import mean_squared_error
 from sklearn.ensemble import GradientBoostingRegressor
+import matplotlib.pyplot as plt
 
 def gen_dataset_from_price(df,step=100):
     """
     df['price'] is required
     """
     price_list = df['price']
-    X = [price_list.values[i:i+step] for i in range(len(price_list))]
-    Y = price_list.values[step:]
+    X = [price_list.values[i:i+step] for i in range(int(len(price_list)/10))]
+    Y = [price_list.values[i+step] for i in range(int(len(price_list)/10))]
     return X,Y
-
 
 def do_train(hdf='/home/yacc/packages/btc-trade-result-history/btc_all.h5',    dataset = 'trade_2014_10_11_with_id'):
     """
     TODO add some comments
     """
     params = {'n_estimators': 500, 'max_depth': 4, 'min_samples_split': 1,'learning_rate': 0.01, 'loss': 'ls'}
-    clf = GradientBoostingRegressor()
+    clf = GradientBoostingRegressor(verbose=True,**params)
 
     h = pd.HDFStore(hdf,'r')
 
     X,y = gen_dataset_from_price(h[dataset],step=200)
+
+    print('data generated.')
     offset = int(len(X) * 0.1)
     X_train, y_train = X[:offset], y[:offset]
     X_test, y_test = X[offset:], y[offset:]
 
     clf.fit(X_train,y_train)
 
-    import matplotlib.pyplot as plt
     plt.plot(y_test,'r-')
     plt.plot(clf.predict(X_test),'b-')
+    plt.show()
 
 if __name__ == '__main__':
     do_train()
---
+++ socketio_client.py
@@ -0,0 +1,51 @@
+from time import sleep
+from socketIO_client import SocketIO
+import json
+
+requestIndex = '1404103038520'
+
+strMsg = {"symbolList":{"lastTimeLine":[{"symbolId":"btccny","pushType":"pushLong"}]},"version":1,"msgType":"reqMsgSubscribe","requestIndex":requestIndex}
+                        #"lastKLine":[{"symbolId":"btccny","pushType":"pushLong","period":"1min"},{"symbolId":"btccny","pushType":"pushLong","period":"5min"},{"symbolId":"btccny","pushType":"pushLong","period":"15min"},{"symbolId":"btccny","pushType":"pushLong","period":"30min"},{"symbolId":"btccny","pushType":"pushLong","period":"60min"},{"symbolId":"btccny","pushType":"pushLong","period":"1day"},{"symbolId":"btccny","pushType":"pushLong","period":"1week"},{"symbolId":"btccny","pushType":"pushLong","period":"1mon"},{"symbolId":"btccny","pushType":"pushLong","period":"1year"}],"marketDepthDiff":[{"symbolId":"btccny","pushType":"pushLong","percent":"10"},{"symbolId":"btccny","pushType":"pushLong","percent":"20"},{"symbolId":"btccny","pushType":"pushLong","percent":"50"},{"symbolId":"btccny","pushType":"pushLong","percent":"80"},{"symbolId":"btccny","pushType":"pushLong","percent":"100"}],"marketDepthTopDiff":[{"symbolId":"btccny","pushType":"pushLong"}],"marketDetail":[{"symbolId":"btccny","pushType":"pushLong"}],"tradeDetail":[{"symbolId":"btccny","pushType":"pushLong"}],"marketOverview":[{"symbolId":"btccny","pushType":"pushLong"}]},
+
+g_isConnect = 0
+options = {'force new connection':True,'reconnection':True}
+
+def on_connect():
+    g_isConnect = 1
+    socket.emit('request',strMsg)
+    print('websocket connect')
+
+def on_disconnect():
+    g_isConnect = 2
+    print('ws disconnect')
+
+def on_reconnect():
+    g_isConnect = 1
+    print('reconnect')
+
+def on_message(data):
+    #requestIndex = data[0]['_id']
+#    print(requestIndex)
+    print(data)
+
+def on_request(data):
+    #print(data)
+    pass
+
+
+socket = SocketIO('hq.huobi.com',80)
+socket.on('connect',on_connect)
+socket.on('disconnect',on_disconnect)
+socket.on('reconnect',on_reconnect)
+socket.on('message',on_message)
+socket.on('request',on_request)
+
+while True:
+    sleep(1.5)
+    #print('requestIndex:{0}'.format(strMsg['requestIndex']))
+    try:
+        strMsg['requestIndex'] = requestIndex
+        socket.emit('request',strMsg)
+        socket.wait(1)
+    except Exception,e:
+        print(e)
